f8c199b6ff2de22a357a97fa661f9cc2164d18ee
pushkarchand <pushkarbc.11ec@gmail.com>
2019-08-03 11:34:24 +0530

    made changes to the server.js file

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
-const express=require('express');
-const app=express();
+const express = require('express');
+const path = require('path');
 
-app.use(express.static(__dirname+'/dist'));
-app.listen(process.env.PORT||8080);
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/Infigradefrontend'));
+
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+'/dist/Infigradefrontend/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
