74f8f43396f6e3bddcfcbe7aa56261f1431d75b2
pushkarchand <pushkarbc.11ec@gmail.com>
2019-08-10 08:01:49 +0530

    completed header and started with the bottom section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { CoursesComponent } from './components/courses/courses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule,
